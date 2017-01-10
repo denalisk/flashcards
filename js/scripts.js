5f995bee168b960a928cb7cbab9349a855e839e0
Sam Kirsch and Shane Villars <sam@>
2017-01-10 11:57:56 -0800

    add color-change effects to the cards and alter the cursor type

--- js/scripts.js
+++ js/scripts.js
@@ -3,10 +3,12 @@ $(document).ready(function() {
     var cardClick = $(this);
     cardClick.toggle();
     cardClick.next().toggle();
+    cardClick.closest(".col-md-3").toggleClass("flashcard-color");
   });
   $(".Definition").click(function() {
     var cardClick = $(this);
     cardClick.toggle();
     cardClick.prev().toggle();
+    cardClick.closest(".col-md-3").toggleClass("flashcard-color");
   });
 });
