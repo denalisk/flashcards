359cef6409f3f4b4111d9b2e8cbb5179a32cf621
Sam Kirsch and Shane Villars <sam@>
2017-01-10 15:54:43 -0800

    add changes to the html organization of the elements to clean up and streamline the look of the site

--- js/scripts.js
+++ js/scripts.js
@@ -3,25 +3,20 @@ $(document).ready(function() {
     var cardClick = $(this);
     cardClick.toggle();
     cardClick.next().toggle();
-    cardClick.closest(".col-md-3").toggleClass("flashcard-color");
   });
   $(".Definition").click(function() {
     var cardClick = $(this);
     cardClick.toggle();
     cardClick.prev().toggle();
-    cardClick.closest(".col-md-3").toggleClass("flashcard-color");
   });
   $(".reset-button").click(function() {
     $(".Definition").hide();
     $(".Term").show();
-    $(".col-md-3").removeClass("flashcard-color");
 
   });
   $(".showall-button").click(function() {
     $(".Term").hide();
     $(".Definition").show();
-    $(".col-md-3").removeClass("flashcard-color");
-    $(".col-md-3").addClass("flashcard-color");
 
   });
 });
